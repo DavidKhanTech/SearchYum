483077915dcbde4e8993793755464f04003fed3b
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-06-29 11:31:41 -0400

    Connected API Data

--- searchyum/src/App.js
+++ searchyum/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const appId = "f546136b";
+  const key = "a92b6c90a77faae0bb4555f47a23924f";
+
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${key}`;
+
+  const data = async () => {
+    const result = await axios.get(url);
+    console.log(result);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 onClick={data}>SearchYum</h1>
+    </>
   );
 }
 
