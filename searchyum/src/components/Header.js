fe5667e3f814657d5c98ef29a1bd2711510c7f71
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-07-02 17:36:46 -0400

    Added CSS

--- searchyum/src/components/Header.js
+++ searchyum/src/components/Header.js
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <div className="header">
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">
+              <a onClick="window.location.reload();">Home</a>
+            </Link>
             <Link to="/Contact">Submit a recipe!</Link>
           </li>
         </ul>
